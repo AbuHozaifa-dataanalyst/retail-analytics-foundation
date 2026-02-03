1218aa32a6ac9ad849b9b6eaf1bc55b6c92736f1
Abu_Hozaifa <abuhozaifa638488@Gmail.com>
2026-02-04 01:03:03 +0300

    Add sales trend analysis script

---
+++ python/sales_trend.py
@@ -0,0 +1,9 @@
+# Simple Python script to sum units sold per product
+import pandas as pd
+
+# Load CSV
+data = pd.read_csv('../data/sample_sales.csv')
+
+# Calculate total units sold per product
+total_units = data.groupby('Product')['Units_Sold'].sum()
+print(total_units)
