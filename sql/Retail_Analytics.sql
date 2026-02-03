22ecae06ade970e5e076d151e4c2d9c8a9945ee0
Abu_Hozaifa <abuhozaifa638488@Gmail.com>
2026-02-04 02:03:05 +0300

    Add files via upload

---
+++ sql/Retail_Analytics.sql
@@ -0,0 +1,14 @@
+USE Retail_Analytics;
+SELECT * FROM Fact_Sales;
+SELECT * FROM Dim_Product;
+SELECT * FROM Dim_Store;
+
+--Total Revenue Per Store
+SELECT
+	st.Store_Name,
+	SUM(s.Amount) AS Total_Revenue
+FROM Fact_Sales s
+JOIN Dim_Store st
+	ON s.Store_ID = st.Store_ID
+GROUP BY st.Store_Name
+ORDER BY Total_Revenue DESC;
